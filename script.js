60f2b57a6127d033a66e2b97b99875d7d5ac4f46
wafiz <wafi21z@hotmail.com>
2016-08-06 15:22:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+	var  person ={
+			
+        		"eyecolor": "brown",
+        		"name":"wafi",
+        		"height":167,
+        		"eyecolor":"black",
+        		"place":"israel?"     	
+        	};
+        		alert(person.name);
+                alert(person.eyecolor);
+                alert(person.height);
+                person.place="ישראל?";
+                person.language="hebrew";
+                console.log(person.language);
+                
+                
+               
